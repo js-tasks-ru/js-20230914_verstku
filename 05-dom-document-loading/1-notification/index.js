3b9a87b482b905ae8aa536b7a35e83acb3ae8eec
Yurii Ruban <verstku@gmail.com>
2024-03-05 19:57:09 +0200

    05-dom-document-loading: 1-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,59 @@
 export default class NotificationMessage {
+  static currentNotification = null;
 
+  constructor(msg = '', {
+    duration = 0,
+    type = ''
+  } = {}) {
+    this.msg = msg;
+    this.duration = duration;
+    this.type = type;
+
+    this.element = this.createNotification();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.msg}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  createNotification() {
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = this.template;
+
+    return wrapper.firstElementChild;
+  }
+
+  show(nodeContainer = document.body) {
+    if (this.constructor.currentNotification) {
+      this.constructor.currentNotification.remove();
+
+    }
+
+    this.constructor.currentNotification =  this.element;
+
+    nodeContainer.append(this.element);
+    setTimeout(() => this.destroy(), this.duration);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
